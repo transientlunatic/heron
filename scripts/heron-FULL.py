ceab79256b0e6a85886e0725799b562ec7f862c9
Daniel Williams <mail@daniel-williams.co.uk>
2017-04-15 15:14:19 -0500

    Added in an MCMC optimiser.

--- scripts/heron-FULL.py
+++ scripts/heron-FULL.py
@@ -134,7 +134,19 @@ def grad_neg_ln_like(p):
     gp.set_vector(p)
     return gp.grad_lnlikelihood(training_y)
 
-minimize(neg_ln_like, gp.get_vector(), method="BFGS", jac=grad_neg_ln_like)
+#minimize(neg_ln_like, gp.get_vector(), method="BFGS", jac=grad_neg_ln_like)
+
+p0 = [gp.get_vector() for i in range(nwalkers)]
+
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob,)
+#burn-in
+pos, prob, state = sampler.run_mcmc(p0, 1000)
+a = sampler.reset()
+a = sampler.run_mcmc(p0, 10000)
+
+import corner
+samples = sampler.chain[:, 100:, :].reshape((-1, ndim))
+fig = corner.corner(samples, labels=cols)
 
 print gp.get_vector()
 
