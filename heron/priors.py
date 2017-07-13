db6cdfcb3271ba1eb3d118b42e340f8b4b82224b
Daniel Williams <mail@daniel-williams.co.uk>
2017-07-13 19:03:30 -0400

    Added the priors module.

---
+++ heron/priors.py
@@ -0,0 +1,18 @@
+class Prior(object):
+    """
+    A prior probability distribution.
+    """
+    pass
+
+class Normal(Prior):
+    """
+    A normal prior probability distribution.
+    """
+    
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+        distro = stats.norm(self.mean, self.std)
+        
+    def logp(self, x):
+        return distro.logpdf(x)
