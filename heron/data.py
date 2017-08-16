Attempts to track down the source of the bug possibly with the normalisation.

heron/data.py
@@ -96,10 +96,11 @@ class Data():
                 if len(label_sigma) == labels.shape[0]:
                     self.label_sigma = self.normalise(label_sigma, "label")
                 else:
+                    
                     raise ValueError("The length of the label uncertainty array doesn't match the data")
             # An array with a fixed sigma for each column
             else:
-                self.label_sigma = np.ones(len(label_sigma))*self.normalise(label_sigma, "label")
+                self.label_sigma = np.ones(len(labels))*self.normalise(label_sigma, "label")
         # If no sigma is provided, assume it equals zero
         else:
             self.label_sigma = np.zeros_like(labels)
@@ -189,7 +190,6 @@ class Data():
         """
         values = []
         for ax in xrange(self.targets.shape[1]):
-            print ax
             values.append(np.median(np.unique(np.diff(self.targets[:, ax])))/2)
         return np.array(values)
 
@@ -226,10 +226,11 @@ class Data():
             dc, range = self.normaliser[name]
         else:
             dc, range = self.calculate_normalisation(data, name)
-
+        print dc, range
         normalised = (data - dc) / range
 
         return normalised
+        #return data
         
 
     def denormalise(self, data, name):
@@ -253,6 +254,7 @@ class Data():
             raise ValueError("There is no normalisation for {}".format(name))
         dc, range = self.normaliser[name]
         return data*range + dc
+        #return data
 
     def add_data(self, targets, labels, target_sigma, label_sigma):
         """

heron/regression.py
@@ -8,6 +8,13 @@ import scipy
 from .training import *
 import copy
 
+def load(filename):
+    """
+    Load a pickled heron Gaussian Process.
+    """
+    import pickle
+    with open(filename, "rb") as gp_file:
+        return pickle.load(gp_file)
 
 class SingleTaskGP(object):
     """
@@ -185,8 +192,7 @@ class SingleTaskGP(object):
         training_y = self.training_y
         new_datum = np.atleast_2d(new_datum).T
         new_datum = self.training_object.normalise(new_datum, "target")
-
-        mean, variance = self.gp.predict(self.training_y, new_datum, return_var=True)
+        mean, variance = self.gp.predict(self.training_y.T[0], new_datum, return_var=True)
         return self.training_object.denormalise(mean, "label"), self.training_object.denormalise(variance, "label")
 
 
@@ -285,7 +291,7 @@ class SingleTaskGP(object):
 
         """
         self.set_hyperparameters(p)
-        return  self.gp.lnlikelihood(self.training_y)
+        return  self.gp.lnlikelihood(self.training_y[:,0])
     
     def neg_ln_likelihood(self, p):
         """
@@ -370,8 +376,7 @@ class SingleTaskGP(object):
         elif method == "MAP":
             MAP = run_training_map(self, metric = metric, **kwargs)
             return MAP
-
-
+        
     def save(self, filename):
         """
         Save the Gaussian Process to a file which can be reloaded later.
@@ -549,15 +554,18 @@ class MultiTaskGP(SingleTaskGP):
            The variance values for the function drawn from the GP.
         """
         means, variances = [], []
-        for gp in self.gps:
-            training_y = gp.training_y
+        for ix, gp in enumerate(self.gps):
+            training_y = gp.training_y[:,ix]
             new_datum = np.atleast_2d(new_datum).T
             new_datum = gp.training_object.normalise(new_datum, "target")
 
-            mean, variance = gp.predict(gp.training_y, new_datum, return_var=True)
-            means.append(gp.training_object.denormalise(mean, "label"))
+            print new_datum
+            
+            mean, variance = gp.predict(training_y, new_datum, return_var=True)
+            means.append(mean) #gp.training_object.denormalise(mean, "label"))
             variances.append(gp.training_object.denormalise(variance, "label"))
         return means, variances
 
 # For backwards compatibility...
-Regressor = SingleTaskGP
+class Regressor(SingleTaskGP):
+    pass