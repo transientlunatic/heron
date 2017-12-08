332855707d3a62916f36a06a701c21b1a994d528
Daniel Williams <daniel.williams@ligo.org>
2017-12-08 16:37:07 +0000

    Added versioneer version racking.

--- heron/__init__.py
+++ heron/__init__.py
@@ -8,3 +8,7 @@ __version__ = "0.0.1"
 __date__ = "2017-06-07"
 __maintainer__ = "Daniel Williams <daniel.williams@ligo.org>"
 
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import versioneer
+version=versioneer.get_version(),
+cmdclass=versioneer.get_cmdclass(),
 
 try:
     from setuptools import setup
