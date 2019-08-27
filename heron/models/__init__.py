2cddd7679750ead816e5b7b4f5d5c4cf550ee37e
Daniel Williams <daniel.williams@ligo.org>
2019-08-27 15:18:26 +0100

    Added some updates to the default model factory.

--- heron/models/__init__.py
+++ heron/models/__init__.py
@@ -5,12 +5,34 @@ class Model(object):
     """
     This is the factory class for statistical models used for waveform generation.
 
-    A model class must expose the following methods:
+    A model class must expose the followi*** TODO Email about Away Dayc_ind = {j:i for i,j in columns.items()}c_ind = {j:i for i,j in columns.items()}ng methods:
     - `distribution` : produce a distribution of waveforms at a given point in the parameter space
     - `mean` : produce a mean waveform at a given point in the parameter space
     - `train` : provide an interface for training the model
     """
 
+    def __init__(self):
+        """
+        Make some default initialisations.
+        These don't really mean anything, but it's important that the variables exist.
+        This __init__ function ought to be usable as a template for other functions, or run from the superclass.
+        """
+
+        # Need to explicitly set the number of dimensions of the training data.
+        # TODO: Decide if this is actually required.
+        self.x_dimensions = 2
+
+        # Need to create a lookup table between the columns in the model
+        # and their location in the training data
+        self.columns = {0: "time",
+                        1: "second quantity"}
+        self.c_ind = {j:i for i,j in self.columns.items()}
+
+        # The training data contains both the x and the y data, so we need
+        # to specify which are parameters.
+        # It's worth noting that right now, time isn't a parameter...
+        self.parameters = ("second quantity",)
+    
     def _process_inputs(self, times, p):
         """
         Apply regularisations and normalisations to any input point dictionary.
