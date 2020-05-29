73294d710e04f0d69c551282df2fd0e142af55e7
Daniel Williams <mail@daniel-williams.co.uk>
2020-05-29 21:51:55 +0100

    Updated tests to use a mock waveform catalogue.

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -3,11 +3,28 @@ Tests for the science testing code.
 """
 
 import unittest
+import numpy as np
 
 from heron import testing
+from elk.waveform import NRWaveform, Timeseries
 from elk.catalogue import NRCatalogue
 
 from heron.models.georgebased import HeronHodlr
+
+class MockWaveform(NRWaveform):
+    def timeseries(self,
+                   total_mass,
+                   sample_rate=4096,
+                   f_low=None,
+                   distance=1,
+                   coa_phase=0,
+                   ma=None,
+                   t_min=None,
+                   t_max=None,
+                   f_ref=None,
+                   t_align=True):
+        return (Timeseries(data=np.random.randn(1000)*1e-19, times=np.linspace(t_min, t_max, 1000)),
+                Timeseries(data=np.random.randn(1000)*1e-19, times=np.linspace(t_min, t_max, 1000)))
 
 class TestTests(unittest.TestCase):
     """
@@ -17,10 +34,22 @@ class TestTests(unittest.TestCase):
     def setUp(self):
         self.model = HeronHodlr()
         self.samples_catalogue = NRCatalogue("GeorgiaTech")
-        self.samples_catalogue.waveforms = self.samples_catalogue.waveforms[:2]
+        mock_waveforms = [
+            MockWaveform("spam", {"q": 1.0,
+                                  "tag": "test",
+                                  "s1x": 0, "s1y": 0, "s1z": 0,
+                                  "s2x": 0, "s2y": 0, "s2z": 0
+            }),
+            MockWaveform("eggs", {"q": 0.8,
+                                  "tag": "test2",
+                                  "s1x": 0, "s1y": 0, "s1z": 0,
+                                  "s2x": 0, "s2y": 0, "s2z": 0
+            })
+            ]
+        
+        self.samples_catalogue.waveforms = mock_waveforms
 
     def test_nrcat_match(self):
         """Test the NR catalogue matcher."""
         matches = testing.nrcat_match(self.model, self.samples_catalogue)
-
         self.assertEqual(len(matches.values()), 2)
