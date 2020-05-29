68435ccb39c9dd1192146bde0903bbc0be532628
Daniel Williams <mail@daniel-williams.co.uk>
2020-05-29 15:05:15 +0100

    Linting the code.

--- heron/models/torchbased.py
+++ heron/models/torchbased.py
@@ -3,51 +3,58 @@ Models which use the GPyTorch GPR package as their backbone.
 """
 
 import math
-import pkg_resources
 from functools import reduce
 import operator
+
+import pkg_resources
 
 import numpy as np
 
 import torch
 import gpytorch
 from gpytorch.kernels import RBFKernel
+from gpytorch.constraints import GreaterThan, LessThan
+
 from matplotlib import pyplot as plt
 
-
 from . import Model
-from .gw import BBHSurrogate, BBHNonSpinSurrogate, HofTSurrogate
+from .gw import BBHSurrogate, HofTSurrogate
 
 DATA_PATH = pkg_resources.resource_filename('heron', 'models/data/')
 
-class HeronCUDA(Model, BBHNonSpinSurrogate, HofTSurrogate):
-    
+class HeronCUDA(Model, BBHSurrogate, HofTSurrogate):
+    """
+    A GPR BBH waveform model which is capable of using CUDA resources.
+    """
+
     time_factor = 1
     x_dimensions = 8
     strain_input_factor = 1e21
-    
-    
+
+
     def __init__(self):
         """
         Construct a CUDA-based waveform model with pyTorch
         """
+
+        super(HeronCUDA, self).__init__()
         self.model, self.likelihood = self.build()
         #
         self.eval()
-        
+
     def eval(self):
         """
         Prepare the model to be evaluated.
         """
         self.model.eval()
         self.likelihood.eval()
-        
+
     def _process_inputs(self, times, p):
         times *= self.time_factor
-        p['mass ratio'] *=100 #= np.log(p['mass ratio']) * 100
+        p['mass ratio'] *= 100 #= np.log(p['mass ratio']) * 100
         return times, p
-    
-    def build():
+
+    def build(self):
         """
         Right now this isn't need by this method
         """
@@ -55,17 +62,24 @@ class HeronCUDA(Model, BBHNonSpinSurrogate, HofTSurrogate):
         def prod(iterable):
             return reduce(operator.mul, iterable)
 
-        mass_kernel = gpytorch.kernels.RBFKernel(active_dims=1, lengthscale_constraint=gpytorch.constraints.GreaterThan(10.))
-        time_kernel = gpytorch.kernels.RBFKernel(active_dims=0, lengthscale_constraint=gpytorch.constraints.GreaterThan(0.1))
-        spin_kernels = [gpytorch.kernels.RBFKernel(active_dims=dimension, lengthscale_constraint=gpytorch.constraints.GreaterThan(7)) for dimension in range(2,8)]
-        
+        mass_kernel = RBFKernel(active_dims=1,
+                                lengthscale_constraint=GreaterThan(10.))
+        time_kernel = RBFKernel(active_dims=0,
+                                lengthscale_constraint=GreaterThan(0.1))
+        spin_kernels = [RBFKernel(active_dims=dimension,
+                                  lengthscale_constraint=GreaterThan(7))
+                        for dimension in range(2, 8)]
+
         class ExactGPModel(gpytorch.models.ExactGP):
+            """
+            Use the GpyTorch Exact GP
+            """
             def __init__(self, train_x, train_y, likelihood):
                 super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
                 self.mean_module = gpytorch.means.ZeroMean()
                 self.covar_module = gpytorch.kernels.ScaleKernel(
                     time_kernel*mass_kernel*prod(spin_kernels),
-                    lengthscale_constraint=gpytorch.constraints.LessThan(0.01) 
+                    lengthscale_constraint=gpytorch.constraints.LessThan(0.01)
                 )
 
             def forward(self, x):
@@ -73,67 +87,104 @@ class HeronCUDA(Model, BBHNonSpinSurrogate, HofTSurrogate):
                 covar_x = self.covar_module(x)
                 return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
-        data = np.genfromtxt(pkg_resources.resource_filename('heron', 'models/data/gt-M60-F1024.dat'))
+        data = np.genfromtxt(pkg_resources.resource_filename('heron',
+                                                             'models/data/gt-M60-F1024.dat'))
 
-        training_x = torch.tensor(data[:,0:-2]*100).float().cuda()
-        training_y = torch.tensor(data[:,-2]*1e21).float().cuda()
-        
-        likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.LessThan(10))
+        training_x = torch.tensor(data[:, 0:-2]*100).float().cuda()
+        training_y = torch.tensor(data[:, -2]*1e21).float().cuda()
+
+        likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=LessThan(10))
         model = ExactGPModel(training_x, training_y, likelihood)
         state_vector = pkg_resources.resource_filename('heron', 'models/data/gt-gpytorch.pth')
 
         model = model.cuda()
         likelihood = likelihood.cuda()
 
-        model.load_state_dict(torch.load(state_vector));
+        model.load_state_dict(torch.load(state_vector))
 
         return model, likelihood
-    
+
     def mean(self, times, p, covariance=False):
+        """
+        Provide the mean waveform and its variance. 
+        Optionally include the covariance.
+
+        Pararameters
+        -------------
+        times : ndarray
+           The times at which the model should be evaluated.
+        p : dict 
+           A dictionary of locations in parameter space where the model should be evaluated.
+        covariance : bool, optional
+           A flag to determine if the whole covariance matrix should be returned.
+
+        Returns
+        -------
+        mean : torch.tensor
+           The mean waveform
+        var : torch.tensor
+            The variance of the waveform
+        cov : torch.tensor, optional
+            The covariance matrix of the waveform. 
+            Only returned if covariance was True.
+        """
+
         covariance_flag = covariance
         times_b = times.copy()
         points = self._generate_eval_matrix(p, times_b)
         points = torch.tensor(points).float().cuda()
         with torch.no_grad(), gpytorch.settings.fast_pred_var():
             f_preds = self.model(points)
-        
-        mean, var = f_preds.mean/self.strain_input_factor, f_preds.variance.detach()/(self.strain_input_factor**2)
-        covariance =  f_preds.covariance_matrix.detach().double()/(self.strain_input_factor**2)
-        
+
+        mean = f_preds.mean/self.strain_input_factor
+        var = f_preds.variance.detach()/(self.strain_input_factor**2)
+        covariance = f_preds.covariance_matrix.detach().double()/(self.strain_input_factor**2)
+
         if covariance_flag:
             return mean, var, covariance
         else:
             return mean, var
-        
+
     def distribution(self, times, p, samples=100):
+        """
+        Return a number of sample waveforms from the GPR distribution.
+        """
         times_b = times.copy()
         points = self._generate_eval_matrix(p, times_b)
         points = torch.tensor(points).float().cuda()
         with torch.no_grad(), gpytorch.settings.fast_pred_var():
             f_preds = self.model(points)
             y_preds = self.likelihood(f_preds)
-        
+
         return y_preds.sample_n(samples)/self.strain_input_factor
 
-    
-    def frequency_domain_waveform(self, p, times = np.linspace(-2, 2, 1000)):
-        mean, var, cov = self.mean(times, p, covariance=True)
+    def frequency_domain_waveform(self, p, times=np.linspace(-2, 2, 1000)):
+        """
+        Return the frequency domain waveform.
+        """
+        mean, _, cov = self.mean(times, p, covariance=True)
 
         strain_f = mean.rfft(1)
         cov_f = cov.rfft(2)
-        
-        uncert_f = torch.stack([torch.diag(cov_f[:,:,0]), torch.diag(cov_f[:,:,1])]).T
+
+        uncert_f = torch.stack([torch.diag(cov_f[:, :, 0]), torch.diag(cov_f[:, :, 1])]).T
 
         return strain_f, uncert_f
-    
-    def time_domain_waveform(self, p, times = np.linspace(-2, 2, 1000)):
+
+    def time_domain_waveform(self, p, times=np.linspace(-2, 2, 1000)):
+        """
+        Return the timedomain waveform.
+        """
         mean, var = self.mean(times, p)
 
         return mean, var
-    
-    def plot_td(self, parameters, times = np.linspace(-2,2,1000), f=None):
+
+    def plot_td(self, parameters, times=np.linspace(-2, 2, 1000), f=None):
+        """
+        Plot the timedomain waveform.
+        """
         if not f:
-            f, ax = plt.subplots(1,1, dpi=300)
+            f, ax = plt.subplots(1, 1, dpi=300)
         else:
             ax = f.axes[0]
 
