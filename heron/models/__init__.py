dd1d69c3399e824460607b61f5b097c36b812be2
Daniel Williams <mail@daniel-williams.co.uk>
2020-07-14 11:45:26 +0100

    Fixed docstring issue.

--- heron/models/__init__.py
+++ heron/models/__init__.py
@@ -5,7 +5,8 @@ class Model(object):
     """
     This is the factory class for statistical models used for waveform generation.
 
-    A model class must expose the followi*** TODO Email about Away Dayc_ind = {j:i for i,j in columns.items()}c_ind = {j:i for i,j in columns.items()}ng methods:
+    A model class must expose the following methods:
+
     - `distribution` : produce a distribution of waveforms at a given point in the parameter space
     - `mean` : produce a mean waveform at a given point in the parameter space
     - `train` : provide an interface for training the model
