bf560cd41534a74eeeca90b49b543e372ae00f68
michael.williams <michael.williams@ligo.org>
2022-08-29 11:32:56 +0100

    Fix total mass prior bound

--- scripts/test-nessai.py
+++ scripts/test-nessai.py
@@ -102,7 +102,7 @@ logger = setup_logger(output=output, label='heron_test', log_level='WARNING')
 priors = {
     "mass ratio": [0.1, 1.0],
     "distance": [100, 1000],
-    "total mass": [20, 60],
+    "total mass": [20, 100],
     "gpstime": [1000.0-1e-1, 1000.0+1e-1],
 }
 
