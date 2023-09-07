9e41090b38f68653af9c05ebed3d639dfaa4780a
Daniel Williams <daniel.williams@ligo.org>
2023-09-07 15:40:54 +0100

    Added initial join detector support.

--- heron/inference.py
+++ heron/inference.py
@@ -61,68 +61,77 @@ def inference(settings):
              }
     
     settings["injection"].update(times)
+
     
     if "injection" in settings:
-        click.echo("Generating injection")
+        injection = {}
+        for ifo in settings["interferometers"]:
+            click.echo(f"Generating injection for {ifo}")
 
-        with report:
-            report += "## Injection"
-            report += settings["injection"]
-
-        psd = heron.injection.psd_from_lalinference(
-            settings["noise model"]["name"],
-            frequencies=heron.injection.frequencies_from_times(times),
-        )
-        noise = heron.injection.create_noise_series(psd, times)
-        signal = models[settings["injection model"]].time_domain_waveform(
-            p=settings["injection"],
-        )
-        print(settings["injection"])
-        print(signal.times)
-
-        detection = Timeseries(data=signal.data + noise, times=signal.times)
-        sos = scipy.signal.butter(
-            10,
-            20,
-            "hp",
-            fs=float(1 / (detection.times[1] - detection.times[0])),
-            output="sos",
-        )
-        detection.data = torch.tensor(
-            scipy.signal.sosfilt(sos, detection.data.cpu()),
-            device=detection.data.device,
-        )
-
-        f, ax = plt.subplots(1, 1, dpi=300)
-        ax.plot(detection.times.cpu(), noise.cpu())
-        ax.plot(detection.times.cpu(), detection.data.cpu())
-        ax.plot(detection.times.cpu(), signal.data.cpu())
-        with report:
-            report += "Injected waveform"
-            report += f
+            settings["injection"]["detector"] = ifo
             
-    heron_likelihood = CUDATimedomainLikelihood(
-        models[settings["waveform"]["model"]],
-        data=detection,
-        detector_prefix="L1",
-        generator_args=times,
-        psd=psd,
-    )
+            with report:
+                report += f"## {ifo} Injection"
+                report += settings["injection"]
 
-    l_times = (heron_likelihood.times)
-    print("l_times", l_times)
-    detection = heron_likelihood._call_model(p=settings['injection'], times=l_times)
+            psd = heron.injection.psd_from_lalinference(
+                settings["noise model"]["name"],
+                frequencies=heron.injection.frequencies_from_times(times),
+            )
+            noise = heron.injection.create_noise_series(psd, times)
+            signal = models[settings["injection model"]].time_domain_waveform(
+                p=settings["injection"],
+            )
+
+            detection = Timeseries(data=signal.data + noise, times=signal.times)
+            sos = scipy.signal.butter(
+                10,
+                20,
+                "hp",
+                fs=float(1 / (detection.times[1] - detection.times[0])),
+                output="sos",
+            )
+            detection.data = torch.tensor(
+                scipy.signal.sosfilt(sos, detection.data.cpu()),
+                device=detection.data.device,
+            )
+
+            f, ax = plt.subplots(1, 1, dpi=300)
+            ax.plot(detection.times.cpu(), noise.cpu())
+            ax.plot(detection.times.cpu(), detection.data.cpu())
+            ax.plot(detection.times.cpu(), signal.data.cpu())
+            with report:
+                report += "Injected waveform"
+                report += f
+            injection[ifo] = detection
+
+    for ifo in settings['interferometers']:
+            
+        likelihood = {}
+        likelihood[ifo] = CUDATimedomainLikelihood(
+            models[settings["waveform"]["model"]],
+            data=injection,
+            detector_prefix="L1",
+            generator_args=times,
+            psd=psd,
+        )
+
+    def joint_likelihood(p):
+        return sum([likelihood[ifo](p) for ifo in likelihood.keys()])
+        
+    l_times = (joint_likelihood[settings['interferometers'][0]].times)
+    detection = joint_likelihood[settings['interferometers'][0]]._call_model(p=settings['injection'], times=l_times)
     f, ax = plt.subplots(1, 1, dpi=300)
     ax.plot(detection.times.cpu(), detection.data.cpu())
 
     with report:
         report += "## Likelihood"
         report += f
-    
-    click.echo(f"Created likelihood on {heron_likelihood.device}")
+
+    click.echo(f"Created likelihood on {joint_likelihood[settings['interferometers'][0]].device}")
 
     nessai_model = HeronSampler(
-        heron_likelihood,
+        joint_likelihood,
         settings["priors"],
         settings["injection"],
         uncertainty=settings["waveform"]["variance"],
