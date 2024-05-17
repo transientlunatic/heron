e76c1ab8ad415b51418c3eb03e6a7d60cab74f73
Daniel Williams <daniel.williams@ligo.org>
2024-05-17 15:51:18 +0100

    Added files I'd forgotten to add.

---
+++ heron/priors.py
@@ -0,0 +1,31 @@
+"""This module contains logic to allow priors to be defined in an efficient manner.
+
+Initially we'll use just the bilby prior handling machinery, but in
+the middle-term it would be good to make this more flexible so that
+other approaches can also be incorporated more easily.
+
+"""
+
+import bilby
+
+KNOWN_PRIORS = {
+    "UniformSourceFrame": bilby.gw.prior.UniformSourceFrame,
+    "UniformInComponentsMassRatio": bilby.gw.prior.UniformInComponentsMassRatio,
+}
+
+
+class PriorDict(bilby.core.prior.PriorDict):
+
+    def from_dictionary(self, dictionary):
+
+        for key, value in dictionary.items():
+            if value["function"] in KNOWN_PRIORS:
+                kind = value.pop("function")
+                print(kind, value)
+                dictionary[key] = KNOWN_PRIORS[kind](**value)
+
+        super().from_dictionary(dictionary)
+
+    @property
+    def names(self):
+        return list(self.keys())
