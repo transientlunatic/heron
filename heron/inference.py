c21980ab94b1bd671d9df02e311de0820dafc1dc
Daniel Williams <daniel.williams@ligo.org>
2024-07-01 18:02:04 +0100

    Add heron as a usable recovery waveform.

--- heron/inference.py
+++ heron/inference.py
@@ -39,9 +39,42 @@ KNOWN_LIKELIHOODS = {
     "TimeDomainLikelihoodModelUncertainty": TimeDomainLikelihoodModelUncertainty,
     "TimedomainLikelihoodModelUncertaintyPyTorch": TimeDomainLikelihoodModelUncertaintyPyTorch
 }
+
+def init_heron():
+    import torch
+    import astropy.units as u
+    from heron.training.makedata import make_manifold, make_optimal_manifold
+    train_data_plus, train_data_cross = make_optimal_manifold(
+        approximant=IMRPhenomPv2,
+        warp_factor=3,
+        varied={"mass_ratio": dict(lower=0.1, upper=1, step=0.05)},
+        fixed={"total_mass": 60*u.solMass,
+               "f_min": 10*u.Hertz,
+               "delta T": 1/(1024*u.Hertz)})
+
+    train_data_plus = torch.tensor(train_data_plus.array(parameter="mass_ratio"), device="cuda", dtype=torch.float32)
+    train_x_plus = train_data_plus[:,[0,1]]
+    train_y_plus = train_data_plus[:,2]
+
+    train_data_cross = torch.tensor(train_data_cross.array(parameter="mass_ratio", component="cross"), device="cuda", dtype=torch.float32)
+    train_x_cross = train_data_cross[:,[0,1]]
+    train_y_cross = train_data_cross[:,2]
+
+    # initialize likelihood and model
+    model = HeronNonSpinningApproximant(train_x_plus=train_x_plus.float(),
+                                        train_y_plus=train_y_plus.float(),
+                                        train_x_cross=train_x_cross.float(),
+                                        train_y_cross=train_y_cross.float(),
+                                        total_mass=(60*u.solMass),
+                                        distance=(1*u.Mpc).to(u.meter).value,
+                                        warp_scale=2,
+                                        training=200,
+                                        )
+    return model
+
 KNOWN_WAVEFORMS = {
     "IMRPhenomPv2": IMRPhenomPv2,
-    "Heron": HeronNonSpinningApproximant,
+    "Heron": init_heron,
 }
 
 
